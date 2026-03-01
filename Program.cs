73c2ad0fadae684c23ff8d6dd0d4ebdb9eed714e
Balogun Fatimah Oyindamola <Fatimah>
2026-03-01 09:10:17 +0100

    my first console app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello world");
+Console.WriteLine("My name is Fatimah");
+
+
+//////////////
+int age = 45;
+string name = "Fatimah";
+double height = 5.11;
+bool ispresent = false;
+
+Console.WriteLine($"my name is {name}");
