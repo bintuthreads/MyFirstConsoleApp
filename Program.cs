e203cd32f01dab5f77a8a453f67aab074ef69fab
Balogun Fatimah Oyindamola <Fatimah>
2026-03-01 20:49:28 +0100

    fatimah classwork march 1st

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello world");
-Console.WriteLine("My name is Fatimah");
+using System.Net.Security;
 
+Console.WriteLine("Hello, This is Fatimah assessment.");
 
-//////////////
-int age = 45;
-string name = "Fatimah";
-double height = 5.11;
-bool ispresent = false;
+string name = "Fatimah Balogun";
+int age = 23;
+double height = 5.21;
+bool isStudent = true;
 
-Console.WriteLine($"my name is {name}");
+/////////////////////
+/// String Concatenation.
+Console.WriteLine($"My name is {name}, i am {age} years old. my height is {height}. Are you from the north? {isStudent}");
+
+/// String Interpolation.
+Console.WriteLine("my name is " + name + ". i am " + age + " years old. My height is " + height + ". Are you from the north?" + isStudent);
