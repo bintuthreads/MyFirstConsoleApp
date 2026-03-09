7f2c0e7de1c3537f76e3b442818fd9472c5fb56f
Balogun Fatimah Oyindamola <Fatimah>
2026-03-09 20:16:53 +0100

    march 8 class

--- obj/Debug/net8.0/MyFirstConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyFirstConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a676cb319bae74a0c1d5594578f5814c0eee5c70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c2ad0fadae684c23ff8d6dd0d4ebdb9eed714e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
