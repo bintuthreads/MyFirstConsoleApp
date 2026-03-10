11f6ac668a6029a8ed2354dd5027d3509b41eaab
Balogun Fatimah Oyindamola <Fatimah>
2026-03-10 12:33:06 +0100

    assignment march 8

--- Program.cs
+++ Program.cs
@@ -1,362 +1,192 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.ComponentModel.Design;
-
-Console.WriteLine("Hello world");
-Console.WriteLine("My name is Fatimah");
-
-
-///////////////*
-/*int age = 45;
-string name = "Fatimah";
-double height = 5.11;*/
-bool ispresent = false;
-//Console.WriteLine($"my name is {name}");
-
-//// march 8 2026
-/// COMPARISON OPERATORS
-// == , egual to
-// != , not equal to
-// > grater than
-// < lessthan
-// >= grater than or equal to
-// <= less than or qual to
-
-//example 
-int a = 10;
-int b = 5;
-
-//Console.WriteLine(a > b); //true
-//Console.WriteLine(a == b);//false
-//Console.WriteLine(a < b); //true
-
-// IF STATEMENTS are conditioner statement
-// if statements are used for conditioner operators;
-
-//if (a > b)
-//{
-//    Console.WriteLine("fatimah was right about a > b to be true");
-//}else if (a == b) ;
-//{
-  //  Console.WriteLine("tutor said what fatimah said was wrong");
-//}
-
-  //max and min for -2,147,483,648 and max 2,147,484,647
-
-//Subject: Operators
-
-// we have four operators : +, - , * , / , %
-
-// int a = 45;
- //int b = 40;
- //int addedResult = a + b;
-// int subtractionResults = a - b;
- 
-//double divisionResults = (double)a / b;
-
-//Console.WriteLine($"Adding a and b values = {addedResult}"); // string interpolation
-//Console.WriteLine($"Subtraction a and b values = {subtractionResults}"); // string interpolation
-//Console.WriteLine($"Division Values  = {divisionResults}");
-
-// there is something we call precision loss  and no precision losss
-
-int pop1 = 200000;
-long pop2 = 9000000000;
-
-long popresult = pop1 + pop2;
-//Console.WriteLine($"Population Total =  {popresult}"); // no precision loss
+﻿//// CHALLENGE 1: Multi-condition Exam Eligibility
+// A student can take the final exam only if they meet at least one of the following conditions:
+//- Paid fees AND has ID card - OR has done matriculation - OR is a citizen
+//Task: - Use boolean variables: paidFees, hasIDCard, hasDoneMatric, isCitizen - Write a
+//logical expression using && and || - Predict the output for: paidFees = true, hasIDCard = //true,
+//hasDoneMatric = false, isCitizen = false - Explain step by step
 
 
-int jumahbalance = 100;
-double sweetprice = 5.75;
+/*bool paidFees = true;
+bool hasIDCard = true;
+bool hasDoneMatric = false;
+bool isCitizen = false;
 
-double jumahamountleft = jumahbalance + sweetprice;
-//Console.WriteLine($"Jumah Total =  {jumahamountleft}"); // no precision loss
-/////////////////
-
-
-
-float value1 = 5.234534635634f;
-decimal value2 = 2.5m;
-
-//decimal valueResult = (decimal)value1 + value2; // precision loss
-float valueResult = value1 + (float)value2; // no precision loss
-
-
-//Console.WriteLine($"Lekan said the total values added = {valueResult}");// 
-
-
-///
-
-double acctbalance = 10.3475;
-decimal acctbalance2 = 5.20m;
-
-double acctresult = acctbalance + (double)acctbalance2;
-
-//Console.WriteLine($"Mariam said the total values added = {acctresult}");// 15.5457
-
-
-// Easy Rules 
-// int + long => safe 
-// int + double => safe 
-//float + double => some precision loss
-//double + decimal => coversion need for any variable type we want
-//float + descimal => coversion need for any variable type we want
-// decimal => best for money
-
-
-
-// Example 
-
-int ab = 10;
-int bc = 5;
-
-int remainder = ab % bc;
-
-//Console.WriteLine($"Remainder values = {remainder}");
-
-int balance = 5000;
-int amounttransfered = 3000;
-
-//int newbalance = balance - amounttransfered; // conventional way 
-balance -= amounttransfered; // prefered way 
-
-//Console.WriteLine($"New balance  = {balance}");
-
-int cd = 2;
-int de = 5;
-
-int jk = cd * de;// conventional way
- cd *= de;
-//Console.WriteLine($"New balance  = {cd}");
-
-// march 8 2026
-
-//Camparison Operators
-// == , equal to.
-// != , not equal to.
-// > , greater than.
-// < , less than.
-// >= . greater than or qual.
-// <= , less than or equal.
-
-// Examples 
-//int a = 5;
-//int b = 5;
-
-//Console.WriteLine(a > b); // true 
-//Console.WriteLine(a == b); // false
-//Console.WriteLine(a != b); // true
-
-// we make use of if and if-else statement for conditional operators;
-
-/*
-if (a > b)
+if (paidFees && hasIDCard || hasDoneMatric || isCitizen)
 {
- Console.WriteLine("Fatimah was right about a > b to be true");
+Console.WriteLine("a student can take final exam");
 }
-else if (a < b)
-{
- Console.WriteLine("Muheeb  was right");
-}
-
 else
 {
- Console.WriteLine("Tutor said what fatimah said was wrong");
-}
-*/
+    Console.WriteLine("a student can not take final exam");
+}*/
+// step 1; paidFees && hasIDCard has the same variables which are "true".
+// true && true == true
+// While hasDoneMatric && isCitizen are "false" values.
+// moreover this makes the full expression to be "true||false||false"
+// step 2: since the expression is "true||false||false",
+// and true is in the statement authomatically the result is "true",
+// because the student has met two conditions (paidFees && hasIDCard)
 
+//Challenge 2 – ACCESS TO SCHOOL PORTAL
 
-// Another example 
+/*A student can access the school portal if: - They have completed registration AND paid
+fees - OR they are a citizen AND have an ID card
+Task: - Use boolean variables: paidFees, hasIDCard, hasDoneRegistration, isCitizen -
+    Evaluate the expression using: paidFees = false, hasIDCard = true,
+hasDoneRegistration = true, isCitizen = false - Explain the output*/
 
-/*int score = 100;
+/*bool paidFees = false;
+bool hasIDCard = true;
+bool hasDoneRegistration = true;
+bool isCitizen = false;
 
-if (score >= 70)
+if (hasIDCard && hasDoneRegistration || paidFees || isCitizen)
 {
- Console.WriteLine("Grade A");
+    Console.WriteLine("a student can access the school portal");
+}
+else
+{
+    Console.WriteLine("a student can not access the school portal");
+}*/
+/// The output statement was "a student can access the school portal" which is true.
+//The output is true because "hasIDCard && hasDoneRegistration"
+    
+    
+/*CHALLENGE3 - GRADING SYSTEM
+Write a program to assign grades based on score: - A if score ≥ 80 - B if score ≥ 60 and < 80 - C 
+if score ≥ 50 and < 60 - Fail if score < 50
+Task: - Use if, else if, and comparison operators - Test for scores: 45, 55, 65, 85 - Explain
+why each score receives the grade*/
+
+/*int score = 45;
+if (score >= 80)
+{
+    Console.WriteLine("Grade A");
 }
 else if (score >= 60)
 {
- Console.WriteLine("Grade B");
+  Console.WriteLine("Grade B"); 
 }
 else if (score >= 50)
 {
- Console.WriteLine("Grade C");
+    Console.WriteLine("Grade C");
 }
+else if (score < 50)
+{
+    Console.WriteLine("Fail");
+}*/
+// score 45 is "fail" because the score is less than 50,
+// of which we already compare it that if it is <50 the result should be "fail"
+
+/*int score = 55;
+if (score >= 80)
+{
+    Console.WriteLine("Grade A");
+}
+else if (score >= 60)
+{
+    Console.WriteLine("Grade B"); 
+}
+else if (score >= 50)
+{
+    Console.WriteLine("Grade C");
+}
+else if (score < 50)
+{
+    Console.WriteLine("Fail");
+}*/
+// score 55 is "Grade C" because the score is more than 50,
+// of which we already compare it that if it is >50 the result should be "Grade C"
+
+/*int score = 65;
+if (score >= 80)
+{
+    Console.WriteLine("Grade A");
+}
+else if (score >= 60)
+{
+    Console.WriteLine("Grade B"); 
+}
+else if (score >= 50)
+{
+    Console.WriteLine("Grade C");
+}
+else if (score < 50)
+{
+    Console.WriteLine("Fail");
+}*/
+// score 65 is "Grade B" because the score is greater than 60,
+// of which we already compare it that if it is >=60 the result should be "Grade B"
+
+/*int score = 85;
+if (score >= 80)
+{
+    Console.WriteLine("Grade A");
+}
+else if (score >= 60)
+{
+    Console.WriteLine("Grade B"); 
+}
+else if (score >= 50)
+{
+    Console.WriteLine("Grade C");
+}
+else if (score < 50)
+{
+    Console.WriteLine("Fail");
+}*/
+// score 85 is "Grade A" because the score is greater than 80,
+// of which we already compare it that if it is >=80 the result should be "Grade A"
+
+////CHALLENGE 4 - LIBARY BORROWING RULES
+/*A student can borrow a book only if: - They have paid library fees AND have a valid ID -
+    OR they are a staff member AND the book is not reserved
+    Task: - Use boolean variables: paidFees, hasIDCard, isStaff, bookReserved - Test with:
+paidFees = true, hasIDCard = true, isStaff = false, bookReserved = false - Explain the
+    output*/
+/*bool paidFees = true;
+bool hasIDCard = true;
+bool isStaff = false;
+bool bookReserved = false;
+
+ if (paidFees && hasIDCard || isStaff || bookReserved)
+{
+ Console.WriteLine("a student can borrow book");
+} 
 else 
 {
- Console.WriteLine("Fail");
+   Console.WriteLine("a student can not borrow book"); 
 }*/
 
-// Logical operators
-// && , means "AND" , this binds true and true values
-// || , means "OR" , only one has to be true
-// ! , means "Not"
+//The output statement was "a student can borrow book" which is true.
+//The output is true because "paidFees && hasIDCard"
 
-// Examples of Logical and comparison operators
+///Challenge 5 – Complex 5-Variable Logic
 
-// using && 
-/*int score = 30;
-
-if (score >= 50 && score <= 100)
-{
- Console.WriteLine("Passed");
-}
-else
-{
- Console.WriteLine("failed");
-}*/
-
-
-/*bool paidfees = true;
-bool hasIcard = true;
-bool hasdonematric = false;
-
-if (paidfees && hasIcard && hasdonematric)
-{
- Console.WriteLine("You can enter the exam hall");
-}
-else
-{
- Console.WriteLine("You cannot enter");
-}*/
-
-
-
-// using ||
-
-/*
-bool isStudent = false;
-bool isSeniorCitizen = false;
-
-if (isStudent || isSeniorCitizen)
-{
- Console.WriteLine("You are qualified for discount");
-}
-else
-{
- Console.WriteLine("You are not qualified for discount");
-}
-*/
-
-
-// combining the && and ||
-
-bool paidfees = true;
-bool hasIcard = true;
-bool hasdonematric = false;
-bool hasdoneRegistartion = false;
+/*Using 5 boolean variables: paidFees, hasIDCard, hasDoneMatric, hasDoneRegistration,
+isCitizen, determine if a student is fully verified for all school processes: - Must either
+    have paid fees AND has ID card AND (has done matric OR has done registration) - OR be
+    a citizen AND has ID card
+Task: - Write the logical expression without brackets first - Evaluate using: paidFees =
+    true, hasIDCard = true, hasDoneMatric = false, hasDoneRegistration = false, isCitizen =
+    false - Show step-by-step evaluation - Explain the result*/
+bool paidFees = true;
+bool hasIDCard = true;
+bool hasDoneMatric = false;
+bool hasDoneRegistration = false;
 bool isCitizen = false;
 
-
-
-// 1
-/*if ((paidfees && hasIcard) || hasdonematric)
-{
- Console.WriteLine("Student can enter exam hall");
+if (paidFees && hasIDCard && (hasDoneMatric || hasDoneRegistration) || isCitizen && hasIDCard)
+{ 
+    Console.WriteLine("student is fully verified");
 }
 else
 {
- Console.WriteLine("Student cannot enter exam hall");
-}*/
-
-//2
-/*if ((hasdoneRegistartion && paidfees) || isCitizen)
-{
- Console.WriteLine("Access Granted");
+ Console.WriteLine("student is not fully verified");   
 }
-else
-{
- Console.WriteLine("Access Denied");
-}*/
-
-// 3
-/*
-if (paidfees && (hasdonematric || hasdoneRegistartion))
-{
- Console.WriteLine("Student is fully verified");
-}
-else
-{
- Console.WriteLine("Student is not verified");
-}
-*/
-
-//4
-
-/*if ((paidfees && hasIcard) && (hasdonematric || hasdoneRegistartion || isCitizen))
-{
- Console.WriteLine("Student cleared");
-}
-else
-{
- Console.WriteLine("Student not cleared");
-}*/
-
-//5 
-
-/*if ((paidfees && hasIcard) && (hasdonematric || hasdoneRegistartion) 
-    || (isCitizen && hasIcard))
-{
- Console.WriteLine("Student can write the final exam");
-}
-else
-{
- Console.WriteLine("Student cannot write the final exam");
-}*/
-
-// 6
-
-/*if (hasdonematric || paidfees && hasIcard)
-{
- Console.WriteLine("Student verified");
-}
-else
-{
- Console.WriteLine("Student not verified");
-}*/
-
-// 7
-
-/*if (paidfees && hasIcard && hasdonematric && hasdoneRegistartion)
-{
- Console.WriteLine("Student fully cleared");
-}
-else
-{
- Console.WriteLine("Student not fully cleared");
-}*/
-
-// 8
-
-/*if (paidfees || hasIcard && hasdonematric || hasdoneRegistartion)
-{
- Console.WriteLine("Approved");
-}
-else
-{
- Console.WriteLine("Rejected");
-}*/
-
-// 9
-
-/*if (paidfees && hasIcard || hasdonematric || isCitizen)
-{
- Console.WriteLine("Student allowed");
-}
-else
-{
- Console.WriteLine("Student not allowed");
-}*/
-
-// 10
-
-
-if (paidfees || hasIcard && hasdonematric || hasdoneRegistartion && isCitizen  )
-{
- Console.WriteLine("Allowed");
-}
-else
-{
- Console.WriteLine("Not Allowed");
-}
+/// the output result is "student is not fully verified"and that because;
+/// hasDoneMatric || hasDoneRegistration = "false ||false" which make the two statements = false
+//likewise, paidFees && hasIDCard = "true && true" which makes the two statements = true
+//while isCitizen && hasIDCard = false && true. 
+// in summmary, the logical statement = true (false)||false && true
+// Therefore, the output answer is false, (student is not fully verified)
